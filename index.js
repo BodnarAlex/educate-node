0972496a5e29f4df5b667c4a423cf6fddb28a181
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2025-03-20 10:52:56 +0200

    feat: start server

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const fs = require('fs');
+const http = require('http');
 
-let tres = fs.readFile('some.txt', 'utf-8', (err, data1) => {
-  fs.writeFile('some.txt', data1 + '\nSome text', (err, data2 => {
-    console.log(`Сработало  ${data1}`);
-  }));
-});
+let server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  //res.end('Hello <b>NODE JS</b>');
+  res.end(`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>NODE JS</title>
+</head>
+<body>
+  <h1>Hello Node!</h1>
+</body>
+</html>`);
+})
 
+const PORT = 3000;
+const HOST = 'localhost';
+
+server.listen(PORT, HOST, () => {
+  console.log(`start server http://${HOST}:${PORT}`)
+})
